614d0a81c5bbd3526630a331d7b49d9bf9d4a434
nyanchan <personal@nyanchangames.com>
2020-10-03 21:18:22 +0900

    Fix fmt

--- src/content.js
+++ src/content.js
@@ -1,23 +1,24 @@
-function killRole(className){
-    // unset the misplaced role="button"
-    const elems = document.getElementsByClassName(className);
-    if(elems.length>0){
-        elems[0].removeAttribute("role");
-    }
+function killRole(className) {
+	// unset the misplaced role="button" (first one only)
+	const elems = document.getElementsByClassName(className);
+	if (elems.length > 0) {
+		elems[0].removeAttribute("role");
+	}
 }
 
-function injectAriaLive(className){
-    const elems = document.getElementsByClassName(className);
-    if(elems.length>0){
-        elems[0].setAttribute("aria-live", "polite");
-    }
+function injectAriaLive(className) {
+	//Injects aria-live on the specified class element (first one only).
+	const elems = document.getElementsByClassName(className);
+	if (elems.length > 0) {
+		elems[0].setAttribute("aria-live", "polite");
+	}
 }
 
 window.addEventListener("load", main, false);
-function main(e){
-setInterval(function(){
-killRole("lobby-page");
-killRole("game-page");
-injectAriaLive("game-log");
-}, 5000);
+function main(e) {
+	setInterval(function () {
+		killRole("lobby-page");
+		killRole("game-page");
+		injectAriaLive("game-log");
+	}, 5000);
 }
